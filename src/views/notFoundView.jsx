b6d70e9f94036d46b96da72af5d2f8430aaa5a36
henrykkebaby <59703378+henrykkebaby@users.noreply.github.com>
2021-12-10 11:07:24 +0100

    404 restyled

--- src/views/notFoundView.jsx
+++ src/views/notFoundView.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import quickquack from '../sounds/quickquack.wav';
+import duckpic from '../localfiles/duck.png';
 
 function NotFoundView(props) {
 
@@ -8,7 +9,11 @@ function NotFoundView(props) {
 
     return (
         <div style={{backgroundColor:"#1a67ab"}}>
-            <p style={{color:"white"}}>404 Not Found</p>
+            <p style={{color:"white", fontSize:"70px"}}>404 Not Found</p>
+            <img src={duckpic} width="120px" height="120px" />
+            <br />
+            <br />
+            <br />
             <Link to = "/"> <button onClick={() => quack.play()}>Goto main page</button> </Link>
         </div>
     )
